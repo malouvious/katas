bdfa57a61b9401dcb676da313027eee1161f2bd6
ludders <daveludlow@hotmail.com>
2014-05-06 21:12:56 +0100

    refactoring

--- checkout_mk2.py
+++ checkout_mk2.py
@@ -4,15 +4,17 @@ class SomeTestMethods(unittest.TestCase):
 	def test_price_of_an_item(self):
 		prices = {"Apple": 0.30}
 		scanner = Scanner(prices)
-		price_a = scanner.scan("Apple")
-		self.assertEqual(price_a, 0.30)
+		scanner.scan("Apple")
+		total = scanner.total()
+		self.assertEqual(total, 0.30)
 
 	def test_price_of_two_apples(self):
 		prices = {"Apple": 0.30}
 		scanner = Scanner(prices)
-		price_a = scanner.scan("Apple")
-		price_a += scanner.scan("Apple")
-		self.assertEqual(price_a, 0.60)
+		scanner.scan("Apple")
+		scanner.scan("Apple")
+		total = scanner.total()
+		self.assertEqual(total, 0.60)
 
 	# def test_two_apples_offer(self):
 	# 	price_of_2_a = 0.30
@@ -22,6 +24,11 @@ class Scanner(object):
 
 	def __init__(self, prices):
 		self.prices = prices
+		self.total_cost = 0
 
 	def scan(self, item):
-		return self.prices[item]
+		self.total_cost += self.prices[item]
+
+	def total(self):
+		return self.total_cost
+
