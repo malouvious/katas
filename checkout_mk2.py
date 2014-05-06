17d69a36e54b3522a7d53e54e5b6e7bdfe1bd822
ludders <daveludlow@hotmail.com>
2014-05-06 20:59:36 +0100

    refactoring

--- checkout_mk2.py
+++ checkout_mk2.py
@@ -1,22 +1,11 @@
 import unittest
 
 class SomeTestMethods(unittest.TestCase):
-	def test_price_of_one_apple(self):
-		prices = {"Apple": 0.30, "Biscuit": 0.50}
+	def test_price_of_an_item(self):
+		prices = {"Apple": 0.30}
 		scanner = Scanner(prices)
 		price_a = scanner.scan("Apple")
 		self.assertEqual(price_a, 0.30)
-
-	def test_price_of_one_biscuits(self):
-		prices = {"Apple": 0.30, "Biscuit": 0.50}
-		scanner = Scanner(prices)
-		price_b = scanner.scan("Biscuit")
-		self.assertEqual(price_b, 0.50)
-
-	# def test_price_of_one_crisps(self):
-	# 	scanner = Scanner()
-	# 	price_c = scanner.scan("Crisps")
-	# 	self.assertEquals(price_c, 1.00)
 
 class Scanner(object):
 
