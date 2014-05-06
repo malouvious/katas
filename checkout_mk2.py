816cfc52122321782454136610a76cec1dcd2803
ludders <daveludlow@hotmail.com>
2014-05-06 21:05:27 +0100

    refactoring

--- checkout_mk2.py
+++ checkout_mk2.py
@@ -7,6 +7,17 @@ class SomeTestMethods(unittest.TestCase):
 		price_a = scanner.scan("Apple")
 		self.assertEqual(price_a, 0.30)
 
+	def test_price_of_two_apples(self):
+		prices = {"Apple": 0.30}
+		scanner = Scanner(prices)
+		price_a = scanner.scan("Apple")
+		price_a += scanner.scan("Apple")
+		self.assertEqual(price_a, 0.60)
+
+	# def test_two_apples_offer(self):
+	# 	price_of_2_a = 0.30
+	# 	self.assertEqual(price_of_2_a, 0.30)
+
 class Scanner(object):
 
 	def __init__(self, prices):
