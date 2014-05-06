916585157b59c3eb768920e10cb1c2ef6e6b0793
ludders <daveludlow@hotmail.com>
2014-05-06 20:43:34 +0100

    refactoring

--- checkout_mk2.py
+++ checkout_mk2.py
@@ -8,13 +8,13 @@ class SomeTestMethods(unittest.TestCase):
 
 	def test_price_of_one_biscuits(self):
 		scanner = Scanner()
-		price_b = scanner.scan_biscuit()
+		price_b = scanner.scan("Biscuit")
 		self.assertEquals(price_b, 0.50)
 
 class Scanner(object):
 
 	def scan(self, item):
-		return 0.30
-
-	def scan_biscuit(self):
-		return 0.50
+		if item == "Apple":
+			return 0.30
+		elif item == "Biscuit":
+			return 0.50
