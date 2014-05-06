7854bf1e10d64a83aa53b1e1056462ad4a6b5dfd
ludders <daveludlow@hotmail.com>
2014-05-06 20:28:36 +0100

    added BISCUIT SCANNER

--- checkout_mk2.py
+++ checkout_mk2.py
@@ -6,7 +6,15 @@ class SomeTestMethods(unittest.TestCase):
 		price_a = scanner.scan_apple()
 		self.assertEquals(price_a, 0.30)
 
+	def test_price_of_one_biscuits(self):
+		scanner = Scanner()
+		price_b = scanner.scan_biscuit()
+		self.assertEquals(price_b, 0.50)
+
 class Scanner(object):
 
 	def scan_apple(self):
 		return 0.30
+
+	def scan_biscuit(self):
+		return 0.50
